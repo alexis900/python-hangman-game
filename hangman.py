e4f901e79cd01603ce1e0eb8e2d6625f1c686437
Alejandro Martin <7107538+alexis900@users.noreply.github.com>
2022-07-20 04:59:09 +0200

    Juego funciona correctamente y comprueba algunas cosas

--- hangman.py
+++ hangman.py
@@ -1,36 +1,64 @@
 from dataclasses import replace
 from random import randint
+import os
+import platform
+from typing import Type
+
+def clearWindow():
+    MY_OS = platform.system()
+    if MY_OS == "Linux" or MY_OS == "Darwin":
+         os.system('clear')
+    elif MY_OS == "Windows":
+        os.system('cls')
 
 
 def normaliceWord(word):
     replacements = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u' }
     for a, b in replacements.items():
-        word = word.replace(a, b).replace('\n', '').upper()
-    return word
+        word = word.replace(a, b).replace('\n', '')
+    return word.upper()
 
 
 def readFile():
     with open('./data.txt', 'r', encoding='utf-8') as f:
-        words = []
-        for lines in f:
-            words.append(normaliceWord(lines))
-
+        words = [lines for lines in f]
         f.close()
-        return words
+    return words
+
 
 def selectWord():
     words = readFile()
     randNum = randint(1, len(words))
-    return words[randNum]
-    
+    return normaliceWord(words[randNum])
+
 
 def game():
     word = selectWord()
+    lenghtWord = len(word)
+    hiddenWord = ['_'] * lenghtWord 
     while True:
-        print(word)
-        return False
-    
+        print(*hiddenWord)
 
+        letter = input('Introduce una letra: ').upper()
+        try:
+            if len(letter) != 1:
+                raise TypeError('Lo siento, solo se puede introducir un carácter')
+        except TypeError as maxChar:
+            print(maxChar)
+        else:
+            try:
+                if not letter.isalpha():
+                    raise TypeError('Lo siento, solo se pueden introducir carácteres')
+            except TypeError as isNotString:
+                print(isNotString)
+            else:
+                for i in range(0, lenghtWord):
+                    if letter == word[i]:
+                        hiddenWord[i] = letter
+                    if hiddenWord.count('_') == 0:
+                        print(f'¡Ganaste! La palabra secreta es {word}')
+                        return False
+            
 
 def run():
     game()
